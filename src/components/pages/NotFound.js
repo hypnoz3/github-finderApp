668329a66a4d906ab1600414275a58966b8be4a6
hypnoz3 <74300099+hypnoz3@users.noreply.github.com>
2021-08-19 14:35:08 +0300

    adding picture to not found page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import notfound from './notfound.png'
 
 const NotFound = () => {
     return (
         <div>
             <h1>Not Found</h1>
+            <img src={notfound} style={{width: "150px"}}alt="" />
             <p className="lead">The page you are looking for does not exist...</p>
         </div>
     )
